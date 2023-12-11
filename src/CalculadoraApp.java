16947b33e088fc92244b98fe5d66ee1472839b39
BluessyJazz <112785703+BluessyJazz@users.noreply.github.com>
2023-12-11 01:39:02 -0500

    Refactor ejecutarOperación, cambios menores

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -9,42 +9,30 @@ public class CalculadoraApp {
             System.out.println("****Calculadora***");
             //Mostramos el menú
             mostrarMenu();
-
-            System.out.print("Elige una opción: ");
+            //Leemos la opción del usuario
             try {
                 var operacion = Integer.parseInt(consola.nextLine());
                 int resultado = 0;
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.print("Escribe el primer valor: ");
-                    var valor1 = Integer.parseInt(consola.nextLine());
-                    System.out.print("Escribe el segundo valor: ");
-                    var valor2 = Integer.parseInt(consola.nextLine());
-                    switch (operacion) {
-                        case 1 -> resultado = valor1 + valor2;
-                        case 2 -> resultado = valor1 - valor2;
-                        case 3 -> resultado = valor1 * valor2;
-                        case 4 -> resultado = valor1 / valor2;
-                        default -> System.out.println("Opción no válida");
-                    }
-                } else if (operacion == 5) {
+                    ejecutarOperacion(operacion, consola);
+                }
+                else if (operacion == 5) {
                     System.out.println("\n----GOOD BYE----");
                     consola.close();
                     break;
-                } else {
-                    System.out.println("Opción no válida" + operacion);
                 }
-                System.out.println("\n|Resultado: " + resultado + "|");
-                System.out.println();
+                else {
+                    System.out.println("\nOpción no válida: " + operacion + "\n");
+                }
             }//fin try
             catch (Exception e) {
-                System.out.println("Opción no válida: " + e.getMessage());
+                System.out.println("\nOpción no válida: " + e.getMessage() + "\n");
                 System.out.println();
             }//fin catch
         }//fin while
     }//fin main
 
     private static void mostrarMenu() {
-        System.out.println("****Calculadora***");
         System.out.println("""
                 1. Suma
                 2. Resta
@@ -52,6 +40,25 @@ public class CalculadoraApp {
                 4. División
                 5. Salir
                 """);
+
+        System.out.print("Elige una opción: ");
     }//fin mostrarMenu
+
+    private static void ejecutarOperacion(int operacion, Scanner consola) {
+        System.out.print("Escribe el primer valor: ");
+        var valor1 = Integer.parseInt(consola.nextLine());
+        System.out.print("Escribe el segundo valor: ");
+        var valor2 = Integer.parseInt(consola.nextLine());
+        int resultado = 0;
+        switch (operacion) {
+            case 1 -> resultado = valor1 + valor2;
+            case 2 -> resultado = valor1 - valor2;
+            case 3 -> resultado = valor1 * valor2;
+            case 4 -> resultado = valor1 / valor2;
+            default -> System.out.println("Opción no válida");
+        }
+        System.out.println("\n|Resultado: " + resultado + "|");
+        System.out.println();
+    }//fin ejecutarOperacion
 
 }//fin clase
